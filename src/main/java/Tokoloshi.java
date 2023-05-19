ab6ba15f057b3a9bdd1cd4f6aeb64e7e7023239a
Jerome <jerome.raghunandan@bmwithub.co.za>
2023-05-19 13:07:21 +0200

    Move the mouse slightly on the x-axis instead of using KeyPress.

--- src/main/java/Tokoloshi.java
+++ src/main/java/Tokoloshi.java
@@ -1,16 +1,20 @@
+
 import java.awt.*;
-import java.awt.event.KeyEvent;
 import java.util.Date;
 
 import static java.lang.Thread.sleep;
 
 public class Tokoloshi {
+
     public static void main(String args[]) throws AWTException, InterruptedException {
         Robot r = new Robot();
-        while(true) {
-            System.out.println("cheating"+new Date());
-            r.keyPress(KeyEvent.VK_F12);
-            r.keyRelease(KeyEvent.VK_F12);
+        while (true) {
+            double xCoordinate = MouseInfo.getPointerInfo().getLocation().getX();
+            double yCoordinate = MouseInfo.getPointerInfo().getLocation().getY();
+            int newXCoordinate = (int) (xCoordinate + 1);
+
+            r.mouseMove(newXCoordinate, (int) yCoordinate);
+            System.out.println("Move X to " + newXCoordinate + ": " + new Date());
             sleep(20000);
         }
     }
